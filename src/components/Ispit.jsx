a8af449a8ae405f8e71dbb70be3d33892cffa8a5
teodora-dot <tbacic.srb5@gmail.com>
2021-02-02 15:08:14 +0100

    Prepravljena komponenta ispit tako da se skraceni naziv prikazuje desno od naziva

--- src/components/Ispit.jsx
+++ src/components/Ispit.jsx
@@ -4,8 +4,8 @@ export default function Ispit(props) {
     return (
         <div className='row mt-2 border justify-content-center rounded bg-light'>
             <div className='col-12 text-center'>
-                <h3>{props.ispit.naziv}</h3>
-                <span>{props.ispit.skraceno}</span>
+                <h3>{props.ispit.naziv} <span>({props.ispit.skraceno})</span></h3>
+
                 <hr />
                 <p>
                     {props.ispit.opis}
